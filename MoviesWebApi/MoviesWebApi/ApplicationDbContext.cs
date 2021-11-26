f77be157d476314ec050eae7b7d0eb3fa34dcb22
Felix Rosales <60873954+FRosales92@users.noreply.github.com>
2021-11-26 15:31:25 +0800

    1- EF tools and EF Sql Servar were installed 2- Entity "Genders" was created 3- "Startup" class was created 4- "Program" class was modied 5- Migrations were configured

---
+++ MoviesWebApi/MoviesWebApi/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MoviesWebApi.Entities;
+
+namespace MoviesWebApi
+{
+    public class ApplicationDbContext: DbContext
+    {
+        public ApplicationDbContext(DbContextOptions options): base(options)
+        {
+        }
+        public DbSet<Genders> Genders { get; set; }
+    }
+}
---
+++ MoviesWebApi/MoviesWebApi/Entities/Genders.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MoviesWebApi.Entities
+{
+    public class Genders
+    {
+        public int Id { get; set; }
+        [Required]
+        [StringLength(40)]
+        public string Name { get; set; }
+    }
+}
